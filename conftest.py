8b8f0e00cf9cb5ea969fbe8f19d1148f99dfa973
AleksMishin <shmurge3@gmail.com>
2025-09-21 14:13:41 +0300

    remove-browser-language-settings-from-conftest

--- conftest.py
+++ conftest.py
@@ -14,25 +14,21 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome', help='Choose browser: chrome or firefox')
-    parser.addoption('--language', action='store', default='ru', help='Choose language: ru or en')
     parser.addoption('--headless', action='store_true', help='Launching the browser in headless mode')
-    parser.addoption('--login', action='store_true', help='Launching the browser with pre conditions: login')
     parser.addoption('--github_actions', action='store_true', help='Launching on Firefox in github actions')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser")
-    user_language = request.config.getoption("language")
     headless = request.config.getoption("--headless")
     github_actions = request.config.getoption("--github_actions")
     browser = None
 
-    with allure.step(f"Запуск браузера: {browser_name}.  Язык браузера: {user_language}"):
+    with allure.step(f"Запуск браузера: {browser_name}."):
         if browser_name == "chrome":
             chrome_options = ChromeOptions()
             chrome_options.add_argument("--disable-notifications")
-            chrome_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
             if headless:
                 with allure.step("Браузер запущен в фоновом режиме"):
                     chrome_options.add_argument('--headless')
@@ -45,7 +41,6 @@ def browser(request):
             firefox_service = FirefoxService(
                 executable_path="/snap/bin/geckodriver")  # если сломается, убрать эту строку
             firefox_options.add_argument("--disable-notifications")
-            firefox_options.set_preference('intl.accept_languages', user_language)
             if headless:
                 with allure.step("Браузер запущен в фоновом режиме"):
                     firefox_options.add_argument("--headless")
